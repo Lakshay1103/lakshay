568f7e7c19259deb35da3982f7bcbb73485835d0
Nmn <nssachdeva2@gmail.com>
2024-01-22 01:46:09 +0530

    first

---
+++ components/nav.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const nav = () => {
+  return (
+    <div className='font-extrabold flex justify-between '>
+      This is from nav.
+    </div>
+  )
+}
+
+export default nav
