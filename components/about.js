2a5eaab94c35f2fd6cd43d61fcd5010179562d02
NmnSach <nssachdeva2@gmail.com>
2024-02-16 01:10:23 +0530

    about fix

--- components/about.js
+++ components/about.js
@@ -5,7 +5,7 @@ const about = () => {
         <div className='flex justify-center items-center mt-20'>    
             <iframe
                 src="https://3oqogyyeekg2mo.embednotionpage.com/About-Me-08db9a121a9f42ab97ee4d6362f9cd0b?no-scroll=true"
-                className="w-[80%] h-[1000px] border-4 border-[#32CD32] rounded-xl p-0"
+                className="w-[80%] h-[800px] border-4 border-[#32CD32] rounded-xl p-0"
             ></iframe>
         </div>
     )
