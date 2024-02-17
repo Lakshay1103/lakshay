9c4ea35c27c545a7e3d1b2c7f89b0af07b89fe8b
NmnSach <nssachdeva2@gmail.com>
2024-02-17 21:45:44 +0530

    vercel fix

--- app/page.js
+++ app/page.js
@@ -1,15 +1,16 @@
 import React from "react";
 import Nav from "../components/nav";
-//import Hero from "../components/hero";
-import Logos from "../components/logos";
+import Hero from "../components/hero";
 import HeroNew from "../components/heroNew";
+import Logos from "../components/logos";
+import About from "../components/about";
 import Steps from "../components/steps";
 import Cards from "../components/cards";
 import Reels from "../components/reels";
+import Carousel from "../components/carousel";
 import FAQ from "../components/faq";
 import Footer from "../components/footer";
-import About from "../components/about";
-import Carousel from "components/carousel";
+
 
 export default function Home() {
   return (
